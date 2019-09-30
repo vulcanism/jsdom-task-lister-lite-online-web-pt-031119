3af03b405ef85cb665e372c916874723b53f99c6
Savannah C <sgcorrell@gmail.com>
2019-09-30 16:35:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+document.querySelector('#submit').addEventListener('click', function(event){
+  let input = document.getElementById('new-task-description').value;
+  let list = document.getElementById('tasks');
+  list.innerHTML += `<li>${input}</li>`;
+  event.preventDefault();
+});
