f348627c5c30949984408cf8742101b2a6861b90
Savannah C <sgcorrell@gmail.com>
2019-09-30 16:44:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function addTask(e){
   e.preventDefault()
-  const newTaskDescription = document.getElementById('new-task-description')
-  const newTask = document.createElement('li')
-  const taskList = document.getElementById('tasks')
+  const newTaskDescription = document.getElementById("new-task-description")
+  const newTask = document.createElement("li")
+  const taskList = document.getElementById("tasks")
   newTask.textContent = newTaskDescription.value
 
-  const deleteTask = document.createElement('button')
-  deleteTask.innerText = 'x'
+  const deleteButton = document.createElement("button")
+  deleteButton.innerText = "X"
+  deleteButton.addEventListener("click", deleteTask)
+  newTask.appendChild(deleteButton)
 
   taskList.appendChild(newTask)
   console.log(e.target)
